72f710c9e95083468ff80bf627b082ae5dbb86ab
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2021-02-04 17:13:52 +0100

    add kv deployment

--- .kubernetes/infra/k8s.tf
+++ .kubernetes/infra/k8s.tf
@@ -61,3 +61,68 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     Environment = "Development"
   }
 }
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "aks-kv" {
+  name                        = "aks-temp-kv"
+  location                    = azurerm_resource_group.k8s.location
+  resource_group_name         = azurerm_resource_group.k8s.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "backup",
+      "create",
+      "decrypt",
+      "delete",
+      "encrypt",
+      "get",
+      "import",
+      "list",
+      "purge",
+      "recover",
+      "restore",
+      "sign",
+      "unwrapKey",
+      "update",
+      "verify",
+      "wrapKey",
+    ]
+
+    secret_permissions = [
+      "backup",
+      "delete",
+      "get",
+      "list",
+      "purge",
+      "recover",
+      "restore",
+      "set",
+    ]
+
+    storage_permissions = [
+      "backup",
+      "delete",
+      "deletesas",
+      "get",
+      "getsas",
+      "list",
+      "listsas",
+      "purge",
+      "recover",
+      "regeneratekey",
+      "set",
+      "setsas",
+      "update",
+    ]
+  }
+}
