b213a878acb14568593f1b75c3532dc706b4153d
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2021-02-06 18:29:46 +0100

    wrong repo name

--- .kubernetes/infra/k8s.tf
+++ .kubernetes/infra/k8s.tf
@@ -70,7 +70,7 @@ resource "local_file" "kubeconfig" {
 }
 
 resource "github_actions_secret" "example_secret" {
-  repository      = "vergiet/hss"
+  repository      = "hss"
   secret_name     = "DNS_NAME"
   plaintext_value = azurerm_kubernetes_cluster.k8s.addon_profile.0.http_application_routing.0.http_application_routing_zone_name
 }
