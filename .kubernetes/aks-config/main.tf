27aaf199c5e9eb103d7eeb4f5804dad1c4325460
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2021-02-09 08:44:32 +0100

    add azurerm_stream_analytics_stream_input_eventhub

--- .kubernetes/aks-config/main.tf
+++ .kubernetes/aks-config/main.tf
@@ -151,3 +151,20 @@ resource "azurerm_stream_analytics_output_eventhub" "example" {
     format   = "Array"
   }
 }
+
+
+resource "azurerm_stream_analytics_stream_input_eventhub" "example" {
+  name                         = "eventhub-stream-input"
+  stream_analytics_job_name    = azurerm_stream_analytics_job.example.name
+  resource_group_name          = azurerm_stream_analytics_job.example.resource_group_name
+  eventhub_consumer_group_name = azurerm_eventhub_consumer_group.example.name
+  eventhub_name                = azurerm_eventhub.example.name
+  servicebus_namespace         = azurerm_eventhub_namespace.example.name
+  shared_access_policy_key     = azurerm_eventhub_namespace.example.default_primary_key
+  shared_access_policy_name    = "RootManageSharedAccessKey"
+
+  serialization {
+    type     = "Json"
+    encoding = "UTF8"
+  }
+}
